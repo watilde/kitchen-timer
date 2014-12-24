356407c16d13cc3652fb75585fdaa2ac9cbf99b3
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-12-24 23:23:27 +0900

    Add timer sound and launcher

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var yargs = require('yargs');
 var baudio = require('baudio');
+
+console.log(yargs);
+
+var b = baudio(function (t, i) {
+  if (i === 1000000) process.exit(0);
+  // taken from some demoscene video
+  return ((
+    (i & 0x71) * Math.floor(i / 3571) * Math.floor(i / 3559)
+  ) % 256) / 128 - 1;
+});
+b.play();
